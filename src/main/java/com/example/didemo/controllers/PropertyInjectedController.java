adc1f311d92b6cd1deb636941b5077da4850cf6a
amadzia <agnieszkamadzia@onet.pl>
2018-06-06 15:00:44 +0200

    Spring profiles

--- src/main/java/com/example/didemo/controllers/PropertyInjectedController.java
+++ src/main/java/com/example/didemo/controllers/PropertyInjectedController.java
@@ -3,12 +3,14 @@ package com.example.didemo.controllers;
 
 import com.example.didemo.services.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class PropertyInjectedController {
 
     @Autowired
+    @Qualifier("greetingServiceImpl")
     public GreetingService greetingServiceImpl;
 
     public String sayHello() {
