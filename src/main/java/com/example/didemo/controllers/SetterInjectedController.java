cdae2c8514e63959fd76567d7b3f612928b179c1
amadzia <agnieszkamadzia@onet.pl>
2018-06-06 14:16:53 +0200

    Qualifiers

--- src/main/java/com/example/didemo/controllers/SetterInjectedController.java
+++ src/main/java/com/example/didemo/controllers/SetterInjectedController.java
@@ -2,6 +2,7 @@ package com.example.didemo.controllers;
 
 import com.example.didemo.services.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -14,6 +15,7 @@ public class SetterInjectedController {
     }
 
     @Autowired
+    @Qualifier("setterGreetingService") //can be use on method level or inside method
     public void setGreetingService(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
