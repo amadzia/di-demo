08721f5be659fad6213114fd826e75dc87485d3a
amadzia <agnieszkamadzia@onet.pl>
2018-06-12 13:42:48 +0200

    Property source

--- src/main/java/com/example/didemo/DiDemoApplication.java
+++ src/main/java/com/example/didemo/DiDemoApplication.java
@@ -1,9 +1,7 @@
 package com.example.didemo;
 
-import com.example.didemo.controllers.ConstructorInjectedController;
 import com.example.didemo.controllers.MyController;
-import com.example.didemo.controllers.PropertyInjectedController;
-import com.example.didemo.controllers.SetterInjectedController;
+import com.example.didemo.examplebeans.FakeDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -17,9 +15,9 @@ public class DiDemoApplication {
 
         MyController controller = (MyController) ctx.getBean("myController");
 
-        System.out.println(controller.hello());
-        System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
-        System.out.println(ctx.getBean(SetterInjectedController.class).sayHello());
-        System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
+        FakeDataSource fakeDataSource = (FakeDataSource) ctx.getBean(FakeDataSource.class);
+
+        System.out.println(fakeDataSource.getUser());
+
     }
 }
